ab0525aa9185f6aeaf22d1aca9041b97580ddc59
Tim Cook <72228729+varanasi2020@users.noreply.github.com>
2025-10-05 16:52:31 +0530

    Create sampleseeddata.sql

---
+++ sampleseeddata.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (title, author, price, stock) VALUES
+('Clean Code', 'Robert C. Martin', 40.0, 10),
+('Effective Java', 'Joshua Bloch', 45.0, 8),
+('Spring in Action', 'Craig Walls', 50.0, 5);
